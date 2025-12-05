b106b57a347851ccb498a342521981205f53e4fb
Vincent Misson <git@misson.cloud>
2025-12-05 23:15:19 +0100

    feat: add cleanup workflow and update resource group name in GitHub variable

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-# output "resource_group_name" {
-#   description = "The name of the resource group where the resources are deployed."
-#   value       = values(module.netperf)[0].resource_group_name
-# }
-
-# output "locationMappings" {
-#   description = "The location mappings used in the deployment."
-#   value       = module.netperf
-# }
+output "resource_group_name" {
+  description = "The name of the resource group where the resources are deployed."
+  value       = var.deploy_infra ? module.infra[0].resource_group_name : ""
+}
