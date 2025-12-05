b36de81dd29155275f4018a619551b291ffd3ad5
Vincent Misson <git@misson.cloud>
2025-12-05 23:19:27 +0100

    fix: uncomment resource group name output in outputs.tf

--- modules/infra/outputs.tf
+++ modules/infra/outputs.tf
@@ -1,4 +1,4 @@
-# output "resource_group_name" {
-#   description = "The name of the resource group where the resources are deployed."
-#   value       = module.resource_group.name
-# }
+output "resource_group_name" {
+  description = "The name of the resource group where the resources are deployed."
+  value       = module.resource_group.name
+}
